c96132474cfc06fbe3f1f3988459f71e6c7bb94e
Бевз Роман Юрьевич <bevz.riu@students.dvfu.ru>
2020-04-27 15:49:30 +1000

    уже вот-вот, почти....

--- Volcano/Pages/GoogleMap.cshtml.cs
+++ Volcano/Pages/GoogleMap.cshtml.cs
@@ -8,28 +8,52 @@ using Microsoft.EntityFrameworkCore;
 using Volcano.Models;
 using System.Data.SqlClient;
 using System.Data;
+using System.Configuration;
+
 
 namespace Volcano.Pages
 {
-    public class GoogleMapModel : PageModel
-    {
-        private readonly ApplicationContext _context;
-        public List<Volcanos> Vulk { get; set; } //это чтобы что-то
+	public class GoogleMapModel : PageModel
+	{
+		private readonly ApplicationContext _context;
+		public List<Volcanos> Vulk { get; set; } //это чтобы что-то
+		[BindProperty]
+		public Volcanos Vulka { get; set; }
 
-        public GoogleMapModel(ApplicationContext db)
-        {
-            _context = db;
-        }
+		public GoogleMapModel(ApplicationContext db)
+		{
+			_context = db;
+			Vulk = _context.Vulk.AsNoTracking().ToList();
+		}
 
-        public void OnGet()
-        {
-            Vulk = _context.Vulk.AsNoTracking().ToList();
-            foreach (var hill in Vulk)
-            {
-                string coordinatesLat = Convert.ToString(hill.NL) + "." + Convert.ToString(hill.NLM);
-                string coordinatesLng = Convert.ToString(hill.EL) + "." + Convert.ToString(hill.ELM);
-                string info = hill.Name;
-            }
-        }
-    }
+		public void OnGet()
+		{
+
+		}
+
+		//public List<Volcanos> GetProperties(List<Volcanos> hill)
+		//{
+		//	hill = _context.Vulk.AsNoTracking().ToList();
+		//	return hill;
+
+		//	//foreach (var hill in Vulk)
+		//	//{
+		//	//	//var Name = hill.Name;
+		//	//	//var EL = hill.EL;
+		//	//	//var ELM = hill.ELM;
+		//	//	//var NL = hill.NL;
+		//	//	//var NLM = hill.NLM;
+		//	//}
+		//}
+		public IActionResult OnPostFranky()
+		{
+			string name = Request.Query["name"];
+			var order = new Volcanos
+			{
+				Name = name
+			};
+			_context.Vulk.Find(order);
+			return RedirectToPage("GoogleMap");
+		}
+	}
 }
