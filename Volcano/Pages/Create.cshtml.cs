b9faf0bdef0e64e716cb23fe2305a4b038cb14c3
Rimuto <52855504+Rimuto@users.noreply.github.com>
2020-04-29 14:34:19 +1000

    Этап 2

--- Volcano/Pages/Create.cshtml.cs
+++ Volcano/Pages/Create.cshtml.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,6 +8,9 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using Volcano.Models;
 using Microsoft.Extensions.Logging;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Collections;
+using System.Text;
 
 namespace Volcano.Pages
 {
